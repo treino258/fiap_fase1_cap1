d7d756bfb70608cc9c3c9412738a87f2b50ae363
Lucas B Francelino <francelino25lucas@gmail.com>
2025-03-16 11:22:03 -0300

    Menu aplicação R

--- objetivo_a/culturas.py
+++ objetivo_a/culturas.py
@@ -11,17 +11,17 @@ class Culturas:
     cultura_1 = []
     cultura_2 = []
 
-    def append_cultura_1(self):
-        self.cultura_1.append({
-            "Tipo": tipo,
-            "Área": area,
-        })
-
-
-    def from_json(self, json):
-        self.cultura_1 = json['cultura1']
-        self.cultura_2 = json['cultura2']
-
-        with open('data.json') as f:
-            data = json.load(f)
-            self.from_json(data)
+    # def append_cultura_1(self):
+    #     self.cultura_1.append({
+    #         "Tipo": tipo,
+    #         "Área": area,
+    #     })
+    #
+    #
+    # def from_json(self, json):
+    #     self.cultura_1 = json['cultura1']
+    #     self.cultura_2 = json['cultura2']
+    #
+    #     with open('data.json') as f:
+    #         data = json.load(f)
+    #         self.from_json(data)
