9babe2c66e2578802a534a75835b2ae6cab39844
Lucas B Francelino <francelino25lucas@gmail.com>
2025-03-16 13:01:51 -0300

    Menu api metereológica

--- ir_alem/api_metereologica.R
+++ ir_alem/api_metereologica.R
@@ -9,20 +9,77 @@ library('tidyr', lib.loc = "ir_alem/lib")
 
 #Repo: https://github.com/tpisel/openmeteo
 
-if (sys.nframe() == 0) {
+timezone <- 'America/Sao_Paulo'
 
- weather_now('Sao Paulo', timezone = 'America/Sao_Paulo')
- weather_forecast('Sao Paulo',
-                  start = Sys.Date(),
-                  end = Sys.Date()+ 7,
-                  daily = "temperature_2m_max",
-                  timezone = 'America/Sao_Paulo')
- weather_history('Sao Paulo',
+sair <- function(){
+
+    sair <- tolower(trimws(readline(prompt = "Tem certeza que deseja sair? (s/n)")))
+
+    if (sair == 's'){
+        quit(save = "no")
+    } else if (sair == 'n'){
+        print("voltando ao menu")
+    } else {
+        print("Opção invalida, voltando ao menu")
+    }
+}
+
+obter_nome_cidade <- function(){
+  cidade <- tolower(trimws(readline(prompt = "Digite o nome da cidade: ")))
+  return(cidade)
+}
+
+previsao_do_tempo <- function(cidade){
+  cidade <- obter_nome_cidade()
+  weather <- weather_now(cidade, timezone = timezone)
+  print(weather)
+}
+
+previsao_proxima_semana <- function(cidade){
+  cidade <- obter_nome_cidade()
+  weather <- weather_forecast(cidade,
+                   start = Sys.Date(),
+                   end = Sys.Date()+ 7,
+                   daily = "temperature_2m_max",
+                   timezone = timezone)
+  print(weather)
+}
+
+previsao_semana_passada <- function(cidade){
+  cidade <- obter_nome_cidade()
+  weather <- weather_history(cidade,
                   start = Sys.Date() - 8,
                   end = Sys.Date() - 1,
                   daily = "temperature_2m_max",
-                 timezone = 'America/Sao_Paulo')
- # weather_variables()
+                  timezone = timezone)
+  print(weather)
+}
+
+menu_weather <- function(){
+  while(TRUE){
+    cat(strrep("-", 20), "\n")
+    cat(sprintf("%10s\n", "Menu"))
+    cat(strrep("-", 20), "\n")
+    print("1 - Previsão do tempo de hoje")
+    print("2 - Previsão do tempo da proxima semana")
+    print("3 - Previsao do tempo da semana passada")
+    print("4 - Sair")
+
+    opcao <- trimws(readline(prompt = "Digite a opcao: "))
+
+    switch(opcao,
+           '1' = previsao_do_tempo(),
+           '2' = previsao_proxima_semana(),
+           '3' = previsao_semana_passada(),
+           '4' = sair(),
+           print('Opcao invalida')
+    )
+  }
+}
 
 
+
+
+if (sys.nframe() == 0) {
+  menu_weather()
 }
